5da638aeab63b575ccd73473a24f0c64257c88d0
jrluiscarvalho <jrluiscarvalho@gmail.com>
2017-07-16 18:07:57 -0300

    templateView

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,11 +3,14 @@ class NegociacaoController {
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor(){
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event){
@@ -21,12 +24,7 @@ class NegociacaoController {
 
         this._negociacoes.adiciona(negociacao);
 
-        this._negociacoes.paraArray().forEach(n => {
-            console.log(n.data);
-            console.log(n.quantidade);
-            console.log(n.valor);
-        })
+        this._negociacoesView.update(this._negociacoes);
 
-        console.log(negociacao);
     }
 }
